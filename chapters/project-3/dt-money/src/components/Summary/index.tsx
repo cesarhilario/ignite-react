244b801479bc6d5e929c4680fd6f9a67743be1ff
César Hilário <cesar.hilario.dev@gmail.com>
2023-04-23 20:19:50 -0300

    feat: summary reduce

--- chapters/project-3/dt-money/src/components/Summary/index.tsx
+++ chapters/project-3/dt-money/src/components/Summary/index.tsx
@@ -6,6 +6,25 @@ import { TransactionsContext } from '../../contexts/TransactionsContext';
 export const Summary = () => {
   const { transactions } = useContext(TransactionsContext);
 
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  );
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -14,7 +33,7 @@ export const Summary = () => {
           <ArrowCircleUp size={32} color='#00b37e' />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
@@ -22,7 +41,7 @@ export const Summary = () => {
           <ArrowCircleDown size={32} color='#f75a68' />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
       <SummaryCard variant='green'>
         <header>
@@ -30,7 +49,7 @@ export const Summary = () => {
           <CurrencyDollar size={32} color='#fff' />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
