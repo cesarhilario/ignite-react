0b290cf1b7ead34f10e26a916cb2e2001c72bce3
César Hilário <cesar.hilario.dev@gmail.com>
2023-04-23 19:09:44 -0300

    feat: TransactionType component

--- chapters/project-3/dt-money/src/components/NewTransactionModal/index.tsx
+++ chapters/project-3/dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { CloseButton, Content, Overlay } from './styles';
-import { X } from 'phosphor-react';
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from './styles';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
 
 export const NewTransactionModal = () => {
   return (
@@ -18,10 +24,19 @@ export const NewTransactionModal = () => {
             <input type='number' placeholder='Preço' required />
             <input type='text' placeholder='Categoria' required />
 
+            <TransactionType>
+              <TransactionTypeButton variant='income' value='income'>
+                <ArrowCircleUp size={24} />
+                Entrada
+              </TransactionTypeButton>
+              <TransactionTypeButton variant='outcome' value='outcome'>
+                <ArrowCircleDown size={24} />
+                Saída
+              </TransactionTypeButton>
+            </TransactionType>
+
             <button type='submit'>Cadastrar</button>
           </form>
-
-          <Dialog.Close />
         </Content>
       </Overlay>
     </Dialog.Portal>
